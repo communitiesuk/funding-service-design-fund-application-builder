007e91168a2bf74dd0f49ad811014745b1924560
NarenderRajuB <bnraju81@gmail.com>
2024-09-13 09:44:00 +0100

    remove backlinktext references

--- app/export_config/generate_fund_round_form_jsons.py
+++ app/export_config/generate_fund_round_form_jsons.py
@@ -14,7 +14,6 @@ form_schema = {
         "name": {"type": "string"},
         "metadata": {"type": "object"},
         "startPage": {"type": "string"},
-        "backLinkText": {"type": "string"},
         "sections": {"type": "array"},
         "pages": {
             "type": "array",
@@ -59,7 +58,6 @@ form_schema = {
     "required": [
         "metadata",
         "startPage",
-        "backLinkText",
         "pages",
         "lists",
         "conditions",
