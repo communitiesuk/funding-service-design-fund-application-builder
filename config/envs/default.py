65fc96b58b9cf33b78774468a053314873a39cf7
srh-sloan <srh-sloan@users.noreply.github.com>
2024-10-17 09:16:04 +0100

    Fs 4724 fix temp path (#72)
    * Updating temp file location to use config
    
    * Fix path errors

--- config/envs/default.py
+++ config/envs/default.py
@@ -1,6 +1,7 @@
 import logging
 from os import environ
 from os import getenv
+from pathlib import Path
 
 from fsd_utils import CommonConfig
 from fsd_utils import configclass
@@ -18,3 +19,5 @@ class DefaultConfig(object):
     FORM_RUNNER_URL = getenv("FORM_RUNNER_INTERNAL_HOST", "http://form-runner:3009")
     FORM_RUNNER_URL_REDIRECT = getenv("FORM_RUNNER_EXTERNAL_HOST", "http://localhost:3009")
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+
+    TEMP_FILE_PATH=Path("/tmp")
