feb8e2d74c123b2846ef830e6893400da246249f
srh-sloan <srh-sloan@users.noreply.github.com>
2024-10-21 10:02:13 +0100

    increasing length of short name and making description a multiline field (#83)

--- app/blueprints/fund_builder/forms/fund.py
+++ app/blueprints/fund_builder/forms/fund.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import HiddenField
 from wtforms import RadioField
 from wtforms import StringField
+from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 from wtforms.validators import Length
 
@@ -10,6 +11,6 @@ class FundForm(FlaskForm):
     fund_id = HiddenField("Fund ID")
     name_en = StringField("Name", validators=[DataRequired()])
     title_en = StringField("Title", validators=[DataRequired()])
-    short_name = StringField("Short Name", validators=[DataRequired(), Length(max=6)])
-    description_en = StringField("Description", validators=[DataRequired()])
+    short_name = StringField("Short Name", validators=[DataRequired(), Length(max=10)])
+    description_en = TextAreaField("Description", validators=[DataRequired()])
     welsh_available = RadioField("Welsh Available", choices=[("true", "Yes"), ("false", "No")], default="false")
