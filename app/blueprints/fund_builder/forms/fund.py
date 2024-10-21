7ac68295895e0b79a4c6ae0717d75352925285d0
srh-sloan <srh-sloan@users.noreply.github.com>
2024-10-21 10:03:44 +0100

    Fs 4749 add funding type (#78)
    * fs-4749 adding funding_type to model and screens
    
    * fs-4749 adding funding_type to exports
    
    * Update app/db/migrations/versions/~2024_10_18_1008-95a6033bea37_.py
    
    Co-authored-by: Steven Fountain <StevenFountain@live.co.uk>
    
    * review comments
    
    ---------
    
    Co-authored-by: Steven Fountain <StevenFountain@live.co.uk>

--- app/blueprints/fund_builder/forms/fund.py
+++ app/blueprints/fund_builder/forms/fund.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from flask_wtf import FlaskForm
 from wtforms import HiddenField
 from wtforms import RadioField
@@ -5,6 +7,27 @@ from wtforms import StringField
 from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 from wtforms.validators import Length
+
+from app.db.models.fund import FundingType
+
+
+class GovUkRadioEnumField(RadioField):
+    source_enum: Enum
+    gov_uk_choices: list
+
+    def __init__(self, name: str, _prefix, _translations, label: str, _form, source_enum: Enum):
+        super().__init__(
+            name=name,
+            label=label,
+            _form=_form,
+            _prefix=_prefix,
+            _translations=_translations,
+            choices=[(value.name, value.value) for value in source_enum],
+        )
+        self.source_enum = source_enum
+        self.gov_uk_choices = [
+            {"text": value.get_text_for_display(), "value": value.value} for value in self.source_enum
+        ]
 
 
 class FundForm(FlaskForm):
@@ -14,3 +37,4 @@ class FundForm(FlaskForm):
     short_name = StringField("Short Name", validators=[DataRequired(), Length(max=10)])
     description_en = TextAreaField("Description", validators=[DataRequired()])
     welsh_available = RadioField("Welsh Available", choices=[("true", "Yes"), ("false", "No")], default="false")
+    funding_type = GovUkRadioEnumField(label="Funding Type", source_enum=FundingType)
