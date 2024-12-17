146885d32fd609ae773f979f2e2db1f1678bf2a3
Albert Artur Kolozsvari <albertartk@gmail.com>
2024-12-17 21:08:13 +0000

    Fix redirect issue

--- app/blueprints/fund_builder/forms/subcriteria.py
+++ app/blueprints/fund_builder/forms/subcriteria.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, StringField
+from wtforms import HiddenField
+from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
