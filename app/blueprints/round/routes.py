fbda606b3483189b29f9737b7002875d0f818372
William May <wjrm500@gmail.com>
2025-01-03 14:10:25 +0000

    FS-4927 - Separating out round endpoint into create_round and edit_round

--- app/blueprints/round/routes.py
+++ app/blueprints/round/routes.py
@@ -32,36 +32,58 @@ round_bp = Blueprint(
 
 
 @round_bp.route("/create", methods=["GET", "POST"])
-@round_bp.route("/<round_id>", methods=["GET", "POST"])
-def round(round_id=None):
+def create_round():
     """
-    Renders a template to select a fund and add or update a round to that fund. If saved, validates the round form data
-    and saves to DB
+    Renders a template to select a fund and create a new round under that fund.
+    If submitted, validates the round form data and saves a new round to the DB.
     """
     form = RoundForm()
     all_funds = get_all_funds()
-    params = {"all_funds": all_funds_as_govuk_select_items(all_funds)}
-    params["selected_fund_id"] = request.form.get("fund_id", None)
-    params["welsh_availability"] = json.dumps({str(fund.fund_id): fund.welsh_available for fund in all_funds})
 
-    if round_id:
-        existing_round = get_round_by_id(round_id)
-        form = populate_form_with_round_data(existing_round, RoundForm)
+    # Prepare parameters for the template
+    params = {
+        "all_funds": all_funds_as_govuk_select_items(all_funds),
+        "selected_fund_id": request.form.get("fund_id", None),
+        "welsh_availability": json.dumps({str(fund.fund_id): fund.welsh_available for fund in all_funds}),
+        "round_id": None,
+        "form": form,
+    }
 
     if form.validate_on_submit():
-        if round_id:
-            update_existing_round(existing_round, form)
-            flash(f"Updated round {existing_round.title_json['en']}")
-            return redirect(url_for("fund_bp.view_fund", fund_id=existing_round.fund_id))
-
         new_round = create_new_round(form)
         flash(f"Created round {new_round.title_json['en']}")
         return redirect(url_for(INDEX_BP_DASHBOARD))
 
-    params["round_id"] = round_id
-    params["form"] = form
-    error = error_formatter(params["form"])
+    error = error_formatter(form)
+    return render_template("round.html", **params, error=error)
 
+
+@round_bp.route("/<round_id>", methods=["GET", "POST"])
+def edit_round(round_id):
+    """
+    Renders a template to view or update an existing round.
+    If submitted, validates the round form data and updates the existing round in the DB.
+    """
+    existing_round = get_round_by_id(round_id)
+    form = populate_form_with_round_data(existing_round, RoundForm)
+
+    all_funds = get_all_funds()
+
+    # Prepare parameters for the template
+    params = {
+        "all_funds": all_funds_as_govuk_select_items(all_funds),
+        "selected_fund_id": request.form.get("fund_id", None),
+        "welsh_availability": json.dumps({str(fund.fund_id): fund.welsh_available for fund in all_funds}),
+        "round_id": round_id,
+        "form": form,
+    }
+
+    if form.validate_on_submit():
+        update_existing_round(existing_round, form)
+        flash(f"Updated round {existing_round.title_json['en']}")
+        return redirect(url_for("fund_bp.view_fund", fund_id=existing_round.fund_id))
+
+    error = error_formatter(form)
     return render_template("round.html", **params, error=error)
 
 
