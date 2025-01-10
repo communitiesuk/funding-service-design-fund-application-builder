c564a2d70127cd5645fca4aa5e73921cfb966911
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-01-10 18:01:10 +0000

    addressing review comments

--- app/blueprints/fund/services.py
+++ app/blueprints/fund/services.py
@@ -5,14 +5,7 @@ def build_fund_rows(funds: list[Fund]) -> list[dict]:
     rows = []
     for fund in funds:
         row = [
-            {
-                "html": f"""
-                <a class='govuk-link--no-visited-state'
-                href='#'>
-                {fund.name_json['en']}
-                </a>
-                """
-            },
+            {"html": f"""<a class='govuk-link--no-visited-state'href='#'>{fund.name_json['en']}</a>"""},
             {"text": fund.description_json["en"]},
             {"text": fund.funding_type.get_text_for_display()},
         ]
