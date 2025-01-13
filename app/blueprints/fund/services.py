be6cb6893521d44d19bcfaf57b60754883253dd5
NarenderRajuB <bnraju81@gmail.com>
2025-01-13 13:57:09 +0000

    Add grant details page and Api to render grant details
    Add grant details page and Api to render grant details
    
    Api to edit grant details
    
    Updated code to focus on field in edit grant
    
    update tests
    
    use _anchor for fieldfocus
    
    remove f string
    
    refactor template
    
    fix sonar issue
    
    Add todo for moving html code to template
    
    Added missing navigation bar
    
    Add navigation highlight for grants
    
    rename grant title to application heading as per figma
    
    Add welsh fields

--- app/blueprints/fund/services.py
+++ app/blueprints/fund/services.py
@@ -1,3 +1,5 @@
+from flask import url_for
+
 from app.db.models import Fund
 
 
@@ -5,7 +7,11 @@ def build_fund_rows(funds: list[Fund]) -> list[dict]:
     rows = []
     for fund in funds:
         row = [
-            {"html": f"""<a class='govuk-link--no-visited-state'href='#'>{fund.name_json['en']}</a>"""},
+            {
+                # TODO move this html to template and use namespace functionality
+                "html": f"""<a class='govuk-link--no-visited-state'
+                href={url_for("fund_bp.view_fund_details", fund_id=fund.fund_id)}>{fund.name_json["en"]}</a>"""
+            },  # noqa: E501
             {"text": fund.description_json["en"]},
             {"text": fund.funding_type.get_text_for_display()},
         ]
