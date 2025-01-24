e3f265b468c0224f269c3bb512bd52014bd82edb
Narender Raju B <bnraju81@gmail.com>
2025-01-24 09:27:22 +0000

    Update add edit grant flows (#237)
    fix sonar issue
    
    Update flow for edit grant
    
    Address review comments
    
    update homepage navigation
    
    fix sonar issue
    
    update css class for success banner
    
    fix styling

--- app/blueprints/fund/routes.py
+++ app/blueprints/fund/routes.py
@@ -16,6 +16,10 @@ from app.shared.helpers import all_funds_as_govuk_select_items, flash_message
 from app.shared.table_pagination import GovUKTableAndPagination
 
 INDEX_BP_DASHBOARD = "index_bp.dashboard"
+SELECT_GRANT_PAGE = "select_grant"
+APPLICATIONS_DETAIL_PAGE = "view_application"
+FUND_LIST_PAGE = "grants_table"
+FUND_DETAILS_ROUTE = "fund_bp.view_fund_details"
 
 # Blueprint for routes used by v1 of FAB - using the DB
 fund_bp = Blueprint(
@@ -67,17 +71,30 @@ def view_fund_details(fund_id):
     return render_template("fund_details.html", form=form, fund=fund)
 
 
+def _create_fund_get_previous_url(actions):
+    if actions == FUND_LIST_PAGE:
+        return url_for("fund_bp.view_all_funds")
+    if actions == SELECT_GRANT_PAGE:
+        return url_for("application_bp.select_fund")
+
+    return url_for(INDEX_BP_DASHBOARD)
+
+
 @fund_bp.route("/create", methods=["GET", "POST"])
 def create_fund():
     """Creates a new fund"""
     form = FundForm()
 
+    params = {"form": form, "fund_id": None}
+
+    params["prev_nav_url"] = _create_fund_get_previous_url(request.args.get("actions"))
+
     if form.validate_on_submit():
         new_fund = Fund(
-            name_json={"en": form.name_en.data},
-            title_json={"en": form.title_en.data},
-            description_json={"en": form.description_en.data},
-            welsh_available=form.welsh_available.data == "true",
+            name_json={"en": form.name_en.data, "cy": form.name_cy.data},
+            title_json={"en": form.title_en.data, "cy": form.title_cy.data},
+            description_json={"en": form.description_en.data, "cy": form.description_cy.data},
+            welsh_available=form.welsh_available.data,
             short_name=form.short_name.data,
             audit_info={"user": "dummy_user", "timestamp": datetime.now().isoformat(), "action": "create"},
             funding_type=FundingType(form.funding_type.data),
@@ -85,27 +102,47 @@ def create_fund():
                 form.ggis_scheme_reference_number.data if form.ggis_scheme_reference_number.data else ""
             ),
         )
+
         add_fund(new_fund)
+        if form.save_and_return_home.data:
+            flash_message(
+                message="New grant added successfully",
+                href=url_for(FUND_DETAILS_ROUTE, fund_id=new_fund.fund_id),
+                href_display_name=form.name_en.data,
+                next_href=url_for("round_bp.create_round", fund_id=new_fund.fund_id),
+                next_href_display_name="Set up a new application",
+            )
+            return redirect(url_for(INDEX_BP_DASHBOARD))
+
         flash_message(
             message="New grant added successfully",
-            href=url_for("fund_bp.view_fund_details", fund_id=new_fund.fund_id),
+            href=url_for(FUND_DETAILS_ROUTE, fund_id=new_fund.fund_id),
             href_display_name=form.name_en.data,
         )
-        match request.args.get("action") or request.form.get("action"):
-            case "return_home":
-                return redirect(url_for(INDEX_BP_DASHBOARD))
-            case "grants_table":
-                return redirect(url_for("fund_bp.view_all_funds"))
-            case _:
-                return redirect(url_for("round_bp.create_round", fund_id=new_fund.fund_id))
 
-    return render_template("fund.html", form=form, fund_id=None)
+        if request.args.get("actions") == "grants_table":
+            return redirect(url_for("fund_bp.view_all_funds"))
+        return redirect(url_for("round_bp.create_round", fund_id=new_fund.fund_id))
+
+    return render_template("fund.html", **params)
+
+
+def _edit_fund_get_previous_url(actions, fund_id, round_id):
+    if actions == APPLICATIONS_DETAIL_PAGE:
+        return url_for("round_bp.round_details", round_id=round_id)
+
+    return url_for(FUND_DETAILS_ROUTE, fund_id=fund_id)
 
 
 @fund_bp.route("/<uuid:fund_id>/edit", methods=["GET", "POST"])
 def edit_fund(fund_id):
     """Updates an existing fund"""
+
     fund = get_fund_by_id(fund_id)
+    round_id = request.args.get("round_id")
+
+    params = {}
+    prev_nav_url = _edit_fund_get_previous_url(request.args.get("actions"), fund_id, round_id)
 
     if request.method == "GET":
         form = FundForm(
@@ -135,7 +172,7 @@ def edit_fund(fund_id):
         fund.title_json["cy"] = form.title_cy.data
         fund.description_json["en"] = form.description_en.data
         fund.description_json["cy"] = form.description_cy.data
-        fund.welsh_available = form.welsh_available.data == "true"
+        fund.welsh_available = form.welsh_available.data
         fund.short_name = form.short_name.data
         fund.audit_info = {"user": "dummy_user", "timestamp": datetime.now().isoformat(), "action": "update"}
         fund.funding_type = form.funding_type.data
@@ -143,8 +180,10 @@ def edit_fund(fund_id):
             form.ggis_scheme_reference_number.data if form.ggis_scheme_reference_number.data else ""
         )
         update_fund(fund)
-        if request.form.get("action") == "return_home":
-            return redirect(url_for(INDEX_BP_DASHBOARD))
-        return redirect(url_for("fund_bp.view_fund_details", fund_id=fund.fund_id))
 
-    return render_template("fund.html", form=form, fund_id=fund_id)
+        if form.save_and_return_home.data:
+            return redirect(url_for(INDEX_BP_DASHBOARD))
+        return redirect(prev_nav_url)
+
+    params.update({"fund_id": fund_id, "form": form, "prev_nav_url": prev_nav_url})
+    return render_template("fund.html", **params)
