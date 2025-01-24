b6043aa7c515f7989d1aa230276a67e385a3db84
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-01-24 07:38:41 +0000

    bau: changing the selection to have grant name instead of application name

--- app/blueprints/round/routes.py
+++ app/blueprints/round/routes.py
@@ -55,7 +55,7 @@ def select_fund():
     form = SelectFundForm()
     choices = [("", "Select a grant")]
     for fund in get_all_funds():
-        choices.append((str(fund.fund_id), fund.short_name + " - " + fund.title_json["en"]))
+        choices.append((str(fund.fund_id), fund.short_name + " - " + fund.name_json["en"]))
     form.fund_id.choices = choices
     if form.validate_on_submit():
         return redirect(
