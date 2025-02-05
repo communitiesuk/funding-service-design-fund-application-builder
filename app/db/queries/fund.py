2d6ccf1dc063aab2ebbe20666c228ee4f9d7b842
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-02-05 14:23:00 +0000

    delete endpoints for grants and applications

--- app/db/queries/fund.py
+++ app/db/queries/fund.py
@@ -1,8 +1,12 @@
+from f86018f3ab453f90c0c3__mypyc import init_djlint___src
 from flask import current_app
 from sqlalchemy import String, cast, select
+from sqlalchemy.orm import joinedload
 
 from app.db import db
+from app.db.models import Round, Component, Page, Form
 from app.db.models.fund import Fund, Organisation
+from app.db.queries.util import delete_all_related_objects
 
 
 def add_organisation(organisation: Organisation) -> Organisation:
@@ -38,3 +42,34 @@ def get_fund_by_id(id: str) -> Fund:
 
 def get_fund_by_short_name(short_name: str) -> Fund:
     return db.session.query(Fund).filter_by(short_name=short_name).first()
+
+
+def _delete_sections_for_fund_round(fund: Fund):
+    for round_detail in fund.rounds:
+        for section in round_detail.sections:
+            if section:
+                page_ids = [page.page_id for form in section.forms for page in form.pages]
+                form_ids = [form.form_id for form in section.forms]
+                section_ids = [section.section_id]
+
+                delete_all_related_objects(db=db, model=Component, column=Component.page_id, ids=page_ids)
+                delete_all_related_objects(db=db, model=Page, column=Page.form_id, ids=form_ids)
+                delete_all_related_objects(db=db, model=Form, column=Form.section_id, ids=section_ids)
+
+                db.session.delete(section)
+                db.session.commit()
+
+
+def delete_selected_fund(fund_id):
+    fund: Fund = db.session.query(Fund).options(joinedload(Fund.rounds).joinedload(Round.sections)).get(fund_id)
+    if not fund:
+        raise ValueError(f"Fund with id {fund_id} not found")
+    try:
+        _delete_sections_for_fund_round(fund)
+        delete_all_related_objects(db=db, model=Round, column=Round.round_id,
+                                   ids=[round_detail.round_id for round_detail in fund.rounds])
+        delete_all_related_objects(db=db, model=Fund, column=Fund.fund_id, ids=[fund_id])
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(f"Failed to delete fund {fund_id} : Error {e}")
